6adca7cb3333110d0d9f479db5e9488fd95866b2
DANY-DURAND <63962014+DANY-DURAND@users.noreply.github.com>
2024-06-29 15:59:06 +0800

    Initial commit

---
+++ hello_world/main.rs
@@ -0,0 +1,7 @@
+fn main(){
+    hello();
+    println!("Hello, world!");
+}
+fn hello(){
+    println!("Hello, Dany!");
+}
---
+++ second_test/main.rs
@@ -0,0 +1,4 @@
+fn main(){
+    println!("Hello, world!");
+    println!("Salut, le monde!");
+}
